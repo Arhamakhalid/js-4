5a3ed9953c8a9ca17264f9626137587878b1f942
Arhamakhalid <arhamaiyani75@gmail.com>
2022-11-14 02:56:46 +0500

    ques/ans

---
+++ app.js
@@ -0,0 +1,169 @@
+// chapter#01
+
+alert("Error! Please enter a valid password.");
+
+var line1 = "Welcome to JS Land...";
+var line2 = "Happy Coding!";
+var message = line1 + "\n" + line2;
+alert(message);
+
+alert("Welcome to JS Land..");
+
+var line3 = "Happy Coding!";
+var line4 = "Prevent this page from creating additional dailogues";
+var message = line3 + "\n" + line4;
+alert(message);
+
+
+alert("Hello... I can run JS through my web Browser's console");
+console.log("Hello... I can run JS through my web Browser's console");
+
+// chapter#2
+
+var mass = "ARHAMA";
+console.log(mass);
+
+alert("Hello World");
+alert("ARHAMA")
+alert("I'm 20 year old")
+alert("Certified Mobile Application Development")
+
+var line5 = "PIZZA";
+var line6 = "PIZZ";
+var line7 = "PIZ";
+var line8 = "PI";
+var line9 = "P"
+var message = line5 + "\n" + line6 + "\n" + line7 + "\n" + line8 + "\n" + line9;
+alert(message);
+
+alert("My email address is arhamakiyani75@gmail.com");
+alert("I am trying to learn from the Book A smarter way to learn Javascript");
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+document.write("Yah! I can write HTML content through JavaScript");
+
+// chapter03
+var age = "I am 15 year old";
+alert(age);
+
+alert("You have visited this site 14 times");
+document.write("<br /> My birth year is 1990");
+document.write("<br /> Data type of my ddeclared variable is number");
+document.write("<br /><b>John Doe</b> ordered <b>5 T-shirts</b>(s) on XYZ Clothing store");
+
+// chapter04
+var statement1 = "hey!";
+var statement2 = "my name is Arhama";
+var statement3 = " i'm 20 years old";
+
+alert(statement1 + statement2 + statement3);
+
+// legal               illegal
+// var _abc            var @123
+// var abc             var alert
+// var ab12            var 123
+// var _123            var -abc
+
+document.write("<br /><h2>Rules for naming JS variables</h2>");
+document.write("<br />Variables names can only contain, numbers , $ and _. For example : $my_1st Variable ");
+document.write("<br />Variable must begin with a letter. $ or _. For example : $name, _name or name");
+document.write("<br />Variable names are case sensitive");
+document.write("<br />Variable name should not be JS keywords");
+
+// chapter05
+
+var a = 5 + 3
+console.log(a);
+
+var b = 5 - 3
+console.log(b);
+
+var c = 5 * 3
+console.log(c);
+
+var d = 5 / 3
+console.log(d);
+
+var a = 4
+a++
+console.log(a);
+
+var b = 5
+++b
+console.log(b);
+
+var e = 7 + 6
+console.log(e);
+
+var c = 13
+c--
+console.log(c);
+
+var j = 18 / 6
+console.log(j);
+
+var g = 2 - 2
+console.log(g);
+
+var f = 600 * 5
+console.log(f);
+
+for (i = 0; i < 10; i++){
+    console.log("4 X " + (i + 1) + " = " + 4 * (i + 1))
+}
+
+var a = 650
+var b = 3
+var c = a * b
+console.log(c);
+
+var d = 100
+var e = 7
+var f = d * e
+console.log(f);
+
+var k = c + f + 100;
+console.log(k);
+
+document.write("<br /><h2>Marks Sheet</h2>"); 
+document.write("<br />Total marks: 980");
+document.write("<br />Marks obtained: 804"); 
+document.write("<br />Percentage: 82.0408163265306%");
+
+var dollars = 10;
+var riyal = 25;
+// convert into pakistani rupees
+
+var DOLLARS = 104.80 * 10;
+var RIYAL = 28 * 25;
+
+console.log(DOLLARS + RIYAL);
+
+document.write("<br /><h2>Currency in PKR</h2>");
+document.write("<br />Total Currency in PKR : 1748");
+
+var abc = 5 * 10 / 2;
+console.log(abc);
+
+var currentage = 2022;
+var birthyear = 2002;
+var total = 2022 - 2002  ;
+console.log(total);
+
+document.write("<h2>Age Calculator</h2>");
+document.write("<br /> Current age: 2022");
+document.write("<br />Birth Year: 2002 ");
+document.write("<br /> Your Age is: 20");
+
+var age = 20;
+var maxage = 68;
+var amount = 3;
+var total = (68 - 20)* 3
+    console.log(total);
+    
+    document.write("<h2>The LifeTime Supple Calculator</h2>");
+document.write("<br /> fav snack: waffers");
+document.write("<br />current age: 20 ");
+document.write("<br /> Estimate Maximum Age: 70");
+document.write("<br /> Amount of snacks per day: 2");
+document.write("<br /> You will need   waffers to last you until the ripe old age of 70");
+
